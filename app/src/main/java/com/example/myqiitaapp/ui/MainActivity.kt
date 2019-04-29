6f02f92284da823bd9c3c30c328e4b9eccb93f18
momo <mmko5110@gmail.com>
2019-04-29 17:23:38 +0900

    Androidx対応

--- app/src/main/java/com/example/myqiitaapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/myqiitaapp/ui/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.myqiitaapp.ui
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myqiitaapp.R
 
