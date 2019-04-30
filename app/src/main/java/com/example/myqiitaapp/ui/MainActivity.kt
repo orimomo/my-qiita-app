be574d951b5c534dff8bb29ec05f28f69fee4e1a
momo <mmko5110@gmail.com>
2019-04-30 14:47:37 +0900

    Groupieが動くようにした

--- app/src/main/java/com/example/myqiitaapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/myqiitaapp/ui/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.myqiitaapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myqiitaapp.R
+import com.example.myqiitaapp.ui.list.ListItem
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val groupAdapter = GroupAdapter<ViewHolder>()
+        findViewById<RecyclerView>(R.id.recycler_view).adapter = groupAdapter
+
+        val items = listOf("りんご", "みかん", "ぶどう")
+        items.forEach { a ->
+            groupAdapter.add(ListItem(a))
+        }
     }
 }
