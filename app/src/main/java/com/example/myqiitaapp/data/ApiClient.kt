f277157b3a79cae5a9fda735f94f61ad3965472a
momo <mmko5110@gmail.com>
2019-05-03 17:22:27 +0900

    ApiClientファイルを作成し、アダプターなどを設定

---
+++ app/src/main/java/com/example/myqiitaapp/data/ApiClient.kt
@@ -0,0 +1,33 @@
+package com.example.myqiitaapp.data
+
+import com.example.myqiitaapp.BuildConfig
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiClient {
+
+    val moshi: Moshi = Moshi.Builder()
+        .build()
+
+    var retrofit: Retrofit
+
+    init {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .baseUrl(BuildConfig.BASE_URL)
+            .build()
+    }
+
+}
