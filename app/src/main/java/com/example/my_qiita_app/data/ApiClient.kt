8800716c0f2cbe5da501c7a7163e893d905290b9
momo <mmko5110@gmail.com>
2019-05-05 19:09:18 +0900

    okhttpがログを吐き出すように設定

--- app/src/main/java/com/example/my_qiita_app/data/ApiClient.kt
+++ app/src/main/java/com/example/my_qiita_app/data/ApiClient.kt
@@ -4,6 +4,7 @@ import com.example.my_qiita_app.BuildConfig
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
@@ -20,6 +21,13 @@ object ApiClient {
             .connectTimeout(20, TimeUnit.SECONDS)
             .writeTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                if (BuildConfig.DEBUG) {
+                    level = HttpLoggingInterceptor.Level.BODY
+                } else {
+                    // TODO: releaseビルドではログを非表示にする
+                }
+            })
             .build()
 
         retrofit = Retrofit.Builder()
