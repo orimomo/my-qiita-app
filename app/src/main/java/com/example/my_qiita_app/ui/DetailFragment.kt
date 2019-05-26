926986bdf8cee577c689009ce38f2505466f72ac
momo <mmko5110@gmail.com>
2019-05-26 18:11:27 +0900

    detail用のレイアウトとクラスを作成

---
+++ app/src/main/java/com/example/my_qiita_app/ui/DetailFragment.kt
@@ -0,0 +1,37 @@
+package com.example.my_qiita_app.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.my_qiita_app.databinding.FragmentDetailBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+class DetailFragment : Fragment(), CoroutineScope {
+    private lateinit var binding: FragmentDetailBinding
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
+    }
+}
