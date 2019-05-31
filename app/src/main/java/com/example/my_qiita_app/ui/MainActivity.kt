9a8329d8cc63eadd8f487b836e4c4f289a1f01c3
momo <mmko5110@gmail.com>
2019-05-31 22:25:44 +0900

    ref

--- app/src/main/java/com/example/my_qiita_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/my_qiita_app/ui/MainActivity.kt
@@ -3,21 +3,11 @@ package com.example.my_qiita_app.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.my_qiita_app.R
 import com.example.my_qiita_app.databinding.ActivityMainBinding
-import com.example.my_qiita_app.ui.list.ListFragment
-import com.example.my_qiita_app.ui.list.ListViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,6 +24,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-    override fun onSupportNavigateUp()
-            = findNavController(R.id.nav_host_fragment).navigateUp()
-    }
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+}
